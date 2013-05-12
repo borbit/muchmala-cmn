89eac4b52e0addb8be1690b78a7b4368b7ba5838
Serge Borbit <serge.borbit@gmail.com>
2013-05-12 12:42:09 +0300

    It is time...

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.Builder = require('./lib/builder');
+exports.Piece   = require('./lib/piece');
+exports.Users   = require('./lib/users');
+exports.User    = require('./lib/user');
+exports.errors  = require('./lib/errors');
+exports.db      = require('./lib/db');
