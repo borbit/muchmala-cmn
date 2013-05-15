1bd811e3743578bddcc5124c0568b0c2c5a1ea2a
Serge Borbit <serge.borbit@gmail.com>
2013-05-15 10:32:53 +0300

    Improved mapper. Added Puzzle.setPieces method.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ exports.Builder = require('./lib/builder');
 exports.Piece   = require('./lib/piece');
 exports.Users   = require('./lib/users');
 exports.User    = require('./lib/user');
+exports.mapper  = require('./lib/mapper');
 exports.errors  = require('./lib/errors');
 exports.db      = require('./lib/db');
 exports.helpers = require('./lib/helpers');
