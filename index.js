3cc52ed01145550e951b937265d51a7a47f82363
Serge Borbit <serge.borbit@gmail.com>
2013-05-19 12:00:01 +0300

    Added leaderboards.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.Builder = require('./lib/builder');
+exports.Boards  = require('./lib/boards');
 exports.Piece   = require('./lib/piece');
 exports.mapper  = require('./lib/mapper');
 exports.errors  = require('./lib/errors');
